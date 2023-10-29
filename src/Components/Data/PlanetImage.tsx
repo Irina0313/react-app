bd5b40c468ab573a0bd16a5c7426ca4bba3ce982
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-10-29 22:30:32 +0300

    feat: migrate PlanetImage.tsx to function components

--- src/Components/Data/PlanetImage.tsx
+++ src/Components/Data/PlanetImage.tsx
@@ -1,36 +1,28 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './PlanetImage.css';
 
 interface PlanetImageProps {
   planetName: string;
 }
-interface PlanetImageState {
-  imageError: boolean;
-}
-class PlanetImage extends Component<PlanetImageProps, PlanetImageState> {
-  constructor(props: PlanetImageProps) {
-    super(props);
-    this.state = {
-      imageError: false,
-    };
-  }
-  private src: string = `./planets/${this.props.planetName}.jpg`;
-  private noImagesrc: string = './no-image-png-2.png';
 
-  handleImageError = () => {
-    this.setState((prevState) => ({ imageError: !prevState.imageError }));
+function PlanetImage(props: PlanetImageProps) {
+  const planetName: string = props.planetName;
+  const [imageError, setImageError] = useState(false);
+  const src: string = `./planets/${planetName}.jpg`;
+  const noImagesrc: string = './no-image-png-2.png';
+
+  const handleImageError = () => {
+    setImageError(true);
   };
 
-  render() {
-    return (
-      <img
-        src={this.state.imageError ? this.noImagesrc : this.src}
-        alt={this.props.planetName}
-        className="planetImage"
-        onError={this.handleImageError}
-      />
-    );
-  }
+  return (
+    <img
+      src={imageError ? noImagesrc : src}
+      alt={planetName}
+      className="planetImage"
+      onError={handleImageError}
+    />
+  );
 }
 
 export default PlanetImage;
