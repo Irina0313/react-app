82b8c9a4b5a10e9436d7cc97fcc0b4de2a6567f7
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-10-29 22:04:03 +0300

    feat: migrate Data.tsx to function components

--- src/Components/Data/Data.tsx
+++ src/Components/Data/Data.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Planet from './Planet';
 import './Data.css';
 import { PlanetProps } from './Planet';
@@ -7,27 +6,21 @@ interface DataProps {
   planets: PlanetProps[];
 }
 
-class Data extends Component<DataProps> {
-  constructor(props: DataProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <section className="planetsWrapper">
-        {this.props.planets.length > 0 ? (
-          this.props.planets.map((planet) => (
-            <Planet key={planet.name} {...planet} />
-          ))
-        ) : (
-          <h2 className="dataTitle">
-            Sorry... Nothing found <br />
-            Try looking for something else
-          </h2>
-        )}
-      </section>
-    );
-  }
+function Data(props: DataProps) {
+  const planets = props.planets;
+  console.log(planets);
+  return (
+    <section className="planetsWrapper">
+      {planets.length > 0 ? (
+        planets.map((planet) => <Planet key={planet.name} {...planet} />)
+      ) : (
+        <h2 className="dataTitle">
+          Sorry... Nothing found <br />
+          Try looking for something else
+        </h2>
+      )}
+    </section>
+  );
 }
 
 export default Data;
